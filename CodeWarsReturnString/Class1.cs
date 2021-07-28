84f8f8c6723602756dbf161b8e6f170628944d12
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-28 13:14:14 -0600

    Add project files.

---
+++ CodeWarsReturnString/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace CodeWarsReturnString
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(Kata.Greet("Ryan"), Is.EqualTo("Hello, Ryan how are you doing today?"));
+        }
+    }
+   
+
+public static class Kata
+    {
+        public static string Greet(string name)
+        {
+            //if (name == "Ryan")
+            //{
+            //    return "Hello, Ryan how are you doing today?";
+            //}
+            //else
+            //{
+            //    return $"Hello, {name} how are you doing today";
+            //}
+            return $"Hello, {name} how are you doing today?";
+        }
+    }
+}
